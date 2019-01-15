3301db635a7b9e958dab74bda487f8493aee644c
Gerdie <mariakmoy@gmail.com>
2019-01-14 22:04:20 -0800

    Day 8 Part 2 complete

--- day8/solution.py
+++ day8/solution.py
@@ -23,6 +23,17 @@ class Node(object):
         self.children = []
         self.num_metadata = num_metadata
         self.metadata = []
+        self.value = 0
+
+    def calculate_value(self):
+        if not self.children:
+            self.value = sum(self.metadata)
+        else:
+            for metadata in self.metadata:
+                try:
+                    self.value += self.children[metadata - 1].value
+                except IndexError:
+                    pass
 
 
 nodes = []
@@ -30,31 +41,32 @@ nodes = []
 
 def create_node(number_list):
     num_children = number_list[0]
-    print('num children: {}'.format(num_children))
     num_metadata = number_list[1]
-    print('num metadata: {}'.format(num_metadata))
     number_list.pop(0)
     number_list.pop(0)
     node = Node(num_children, num_metadata)
     for c in xrange(num_children):
         node.children.append(create_node(number_list))
     for m in xrange(num_metadata):
-        print('metadata: {}'.format(number_list[0]))
         node.metadata.append(number_list[0])
         number_list.pop(0)
-
+    node.calculate_value()
     return node
 
 
 while numbers:
     nodes.append(create_node(numbers))
 
+root_value = None
 sum_metadata = 0
 while nodes:
     node = nodes[-1]
     nodes.pop(-1)
     sum_metadata += sum(node.metadata)
     nodes.extend(node.children)
+    if root_value is None:
+        root_value = node.value
 
 
 print('Part One: {}'.format(sum_metadata))
+print('Part Two: {}'.format(root_value))
